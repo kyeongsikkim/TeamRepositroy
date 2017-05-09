7ea8f7ab5502434882f806fd20830741d18893b3
Dahye Lee <PrayAnne@idahyeui-MacBook-Pro.local>
2017-05-09 17:21:10 +0900

    유정이 코드

--- callDisplay/CallController.java
+++ callDisplay/CallController.java
@@ -3,12 +3,17 @@ package callDisplay;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
@@ -41,13 +46,36 @@ public class CallController implements Initializable {
                     try {
                         stack.getChildren().clear();
                         Parent parent = FXMLLoader.load(getClass().getResource("telephone.fxml"));
+                        Button btnStar = (Button) parent.lookup("#btnStar");
+                        Button btnSharp = (Button) parent.lookup("#btnSharp");
+                        btnStar.setText("*");
+                        btnSharp.setText("#");
                         stack.getChildren().add(parent);
+                        
                     } catch (IOException ex) {
                     }
                 } else if (newValue == call2) {
                     try {
                         stack.getChildren().clear();
-                        Parent parent = FXMLLoader.load(getClass().getResource("interphone.fxml"));
+                        Parent parent = FXMLLoader.load(getClass().getResource("telephone.fxml"));
+                        Button btnStar = (Button) parent.lookup("#btnStar");
+                        Button btnSharp = (Button) parent.lookup("#btnSharp");
+                        TextField textArea = (TextField) parent.lookup("#textArea");
+                        
+                        btnStar.setText("동");
+                        btnSharp.setText("호수");
+                        btnStar.setOnAction(new EventHandler<ActionEvent>() {
+							@Override
+							public void handle(ActionEvent event) {
+								textArea.appendText("동");
+							}
+                        });
+                        btnSharp.setOnAction(new EventHandler<ActionEvent>() {
+							@Override
+							public void handle(ActionEvent event) {
+								textArea.appendText("호");
+							}
+                        });
                         stack.getChildren().add(parent);
                     } catch (IOException ex) {
                     }
