f95a00a418512401e961a76847c3d081596cc012
Dahye Lee <PrayAnne@218.38.137.27>
2017-05-14 19:39:19 +0900

    mainDisplay 패키지&fx:controller 정리

--- SecurityDisplay_Main/src/mainDisplay/AppMain.java
+++ SecurityDisplay_Main/src/mainDisplay/AppMain.java
@@ -8,22 +8,17 @@ import javafx.stage.Stage;
 
 public class AppMain extends Application {
 
-    public static Stage primaryStage;
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        AppMain.primaryStage = primaryStage;
-
-        Parent parent = FXMLLoader.load(getClass().getResource("mainDisplay.fxml"));
-
-        Scene scene = new Scene(parent);
-        primaryStage.setTitle("AppMain");
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent parent  = FXMLLoader.load(getClass().getResource("security_main.fxml"));
+		Scene scene = new Scene(parent);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("방범");
+		primaryStage.setOnCloseRequest(event -> System.out.println("Close"));
+		primaryStage.show();
+	}
 }
